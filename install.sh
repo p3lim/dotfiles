dabe44617b1eb557ebb03ba1c0a3003146b8bf73
Adrian L Lange <mail@p3lim.net>
2018-03-18 18:14:16 +0100

    Some settings we apply for WSL on Windows as well

--- install.sh
+++ install.sh
@@ -20,71 +20,74 @@ link(){
 }
 
 # symlinks
-link '.bash_profile'
+link 'bin'
 link '.bashrc'
 link '.inputrc'
-link '.xinitrc'
-link '.xprofile'
-link '.xresources'
-
-link 'bin'
 link '.config/bash'
-link '.config/bspwm'
-link '.config/compton'
-link '.config/eventd'
-link '.config/git'
-link '.config/i3'
-link '.config/micro'
-link '.config/rofi'
-link '.config/termite'
-link '.config/gtk-3.0/gtk.css'
-link '.config/sublime-text-3/Packages/C++'
-link '.config/sxhkd'
 
-for file in .config/sublime-text-3/Packages/User/*; do
-	link ".config/sublime-text-3/Packages/User/$(basename $file)"
-done
+if ! [[ "$(uname -r)" = *Microsoft* ]]; then
+	link '.bash_profile'
+	link '.xinitrc'
+	link '.xprofile'
+	link '.xresources'
 
-for file in .config/sublime-text-3/Packages/Default/*; do
-	link ".config/sublime-text-3/Packages/Default/$(basename $file)"
-done
+	link '.config/bspwm'
+	link '.config/compton'
+	link '.config/eventd'
+	link '.config/git'
+	link '.config/i3'
+	link '.config/micro'
+	link '.config/rofi'
+	link '.config/termite'
+	link '.config/gtk-3.0/gtk.css'
+	link '.config/sublime-text-3/Packages/C++'
+	link '.config/sxhkd'
 
-for file in .config/systemd/user/*; do
-	link ".config/systemd/user/$(basename $file)"
-done
+	for file in .config/sublime-text-3/Packages/User/*; do
+		link ".config/sublime-text-3/Packages/User/$(basename $file)"
+	done
 
-link '.mozilla/firefox/profiles.ini'
-link '.mozilla/firefox/custom/chrome'
+	for file in .config/sublime-text-3/Packages/Default/*; do
+		link ".config/sublime-text-3/Packages/Default/$(basename $file)"
+	done
 
-# install a sublime plugin
-git clone https://github.com/p3lim/sublime-paste $HOME/.config/sublime-text-3/Packages/Paste
+	for file in .config/systemd/user/*; do
+		link ".config/systemd/user/$(basename $file)"
+	done
 
-# copy config files
-sudo cp --parents -rb etc /
-sudo cp --parents -rb usr /
-sudo cp --parents -rb root /
+	link '.mozilla/firefox/profiles.ini'
+	link '.mozilla/firefox/custom/chrome'
 
-# use "unpredictable" network interface names
-sudo ln -s /dev/null /etc/systemd/network/99-default.link
+	# install a sublime plugin
+	git clone https://github.com/p3lim/sublime-paste $HOME/.config/sublime-text-3/Packages/Paste
 
-# enable wpa_supplicant on wifi interface
-sudo systemctl enable wpa_supplicant@wlan0.service
+	# copy config files
+	sudo cp --parents -rb etc /
+	sudo cp --parents -rb usr /
+	sudo cp --parents -rb root /
 
-# enable cronie
-sudo systemctl enable cronie.service --now
+	# use "unpredictable" network interface names
+	sudo ln -s /dev/null /etc/systemd/network/99-default.link
 
-# enable sxhkd
-systemctl --user enable sxhkd.service --now
+	# enable wpa_supplicant on wifi interface
+	sudo systemctl enable wpa_supplicant@wlan0.service
 
-# enable eventd
-systemctl --user enable eventd
-systemctl --user start eventd-control.socket eventd.socket
+	# enable cronie
+	sudo systemctl enable cronie.service --now
 
-# enable slock
-sudo systemctl enable slock@$user.service
+	# enable sxhkd
+	systemctl --user enable sxhkd.service --now
 
-# speed up compilation by building in tmpfs
-sudo sed -i '/BUILDDIR/s/^#//g' /etc/makepkg.conf
+	# enable eventd
+	systemctl --user enable eventd
+	systemctl --user start eventd-control.socket eventd.socket
+
+	# enable slock
+	sudo systemctl enable slock@$user.service
+
+	# speed up compilation by building in tmpfs
+	sudo sed -i '/BUILDDIR/s/^#//g' /etc/makepkg.conf
+fi
 
 if [[ -f private/install.sh ]]; then
 	bash private/install.sh
